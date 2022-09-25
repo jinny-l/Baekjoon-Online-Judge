6991d2ca26c36a678c5392a3c3f8fc04372e2842
Jinny-l <jinny.l.dev@gmail.com>
2022-09-26 01:12:41 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 212 ms, Memory: 17744 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int year;
+        year = scanner.nextInt();
+
+        System.out.println(year - 543);
+    }
+}
