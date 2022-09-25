0d2f5c8052ca5a5f3eea16ae0dce57789960c2b4
Jinny-l <jinny.l.dev@gmail.com>
2022-09-26 01:12:36 +0900

    [Bronze V] Title: ??!, Time: 204 ms, Memory: 17644 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        String lastString, ID;
+
+        lastString = "??!";
+        ID = scanner.next();
+        System.out.println(ID+lastString);
+
+    }
+}
