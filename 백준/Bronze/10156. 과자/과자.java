0b32cfd4f1e0a296ddfe41a0c2801ec62077f85d
Jinny-l <jinny.l.dev@gmail.com>
2022-10-02 21:21:32 +0900

    [Bronze IV] Title: 과자, Time: 124 ms, Memory: 14180 KB -BaekjoonHub

---
+++ 백준/Bronze/10156. 과자/과자.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int K = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        br.close();
+
+        int money = K*N - M;
+        if (money < 0) money = 0;
+        System.out.print(money);
+
+    }
+}
