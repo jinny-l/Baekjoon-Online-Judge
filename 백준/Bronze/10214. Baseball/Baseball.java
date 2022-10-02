d43777cd6d6e8a019d73c9a996609ac9dc58d8f0
Jinny-l <jinny.l.dev@gmail.com>
2022-10-02 20:36:09 +0900

    [Bronze III] Title: Baseball, Time: 128 ms, Memory: 14536 KB -BaekjoonHub

--- 백준/Bronze/10214. Baseball/Baseball.java
+++ 백준/Bronze/10214. Baseball/Baseball.java
@@ -3,25 +3,21 @@ import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        
+
         // T 입력
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int T = Integer.parseInt(br.readLine());
 
-        StringTokenizer st;
-        int arr1[] = new int[9];
-        int arr2[] = new int[9];
-        
+
         // 점수 입력
+        StringTokenizer st;
         for (int i = 0; i < T; i++) {
             int Yonsei = 0;
             int Korea = 0;
             for (int j = 0; j < 9; j++) { // 9개의 값 입력과 동시에, 합산 값 저장
                 st = new StringTokenizer(br.readLine(), " ");
-                arr1[j] = Integer.parseInt(st.nextToken());
-                arr2[j] = Integer.parseInt(st.nextToken());
-                Yonsei += arr1[j];
-                Korea += arr2[j];
+                Yonsei += Integer.parseInt(st.nextToken());
+                Korea += Integer.parseInt(st.nextToken());
             }
             if (Yonsei > Korea) System.out.println("Yonsei");
             if (Yonsei < Korea) System.out.println("Korea");
