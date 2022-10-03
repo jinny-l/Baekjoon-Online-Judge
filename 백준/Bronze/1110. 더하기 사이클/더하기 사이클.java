bb3d373c70ee0ae0816d0f176fbf8cd27537528a
Jinny-l <jinny.l.dev@gmail.com>
2022-10-03 22:17:13 +0900

    [Bronze I] Title: 더하기 사이클, Time: 124 ms, Memory: 14196 KB -BaekjoonHub

--- 백준/Bronze/1110. 더하기 사이클/더하기 사이클.java
+++ 백준/Bronze/1110. 더하기 사이클/더하기 사이클.java
@@ -1,28 +1,25 @@
-import java.util.*;
+import java.io.*;
 
 public class Main {
+    public static void main(String[] args) throws IOException {
 
-    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        Scanner scanner = new Scanner(System.in);
-        int N = scanner.nextInt();
+        // N 입력
+        int N = Integer.parseInt(br.readLine());
 
-        int cmp = N;    // 원래 값 저장
-        int cnt = 0;    // 싸이클 수
+        int cnt = 0;  // 싸이클 수
+        int tmp = N;  // 초기값 저장 후, while 문 내에서 임시값 저장
 
         while (true) {
 
-            int N1 = ((N / 10 + N % 10)) % 10; // 1의 자리수
-            int N10 = N % 10 * 10; // 10의 자리수
-            N = N1 + N10;  // 새로운 수
+            tmp = (tmp % 10) * 10 + ((tmp/10 + tmp%10) % 10);
             cnt++;
 
-            if (N == cmp) {
-                break;
-            }
+            if (tmp == N) break;
         }
 
-        System.out.println(cnt);
+        System.out.print(cnt);
 
     }
 }
