19898e408f7bf306ff68d08524940f4b99009b23
Jinny-l <jinny.l.dev@gmail.com>
2023-01-04 18:47:43 +0900

    [Bronze II] Title: 저항, Time: 128 ms, Memory: 14292 KB -BaekjoonHub

--- 백준/Bronze/1076. 저항/저항.java
+++ 백준/Bronze/1076. 저항/저항.java
@@ -1,52 +1,37 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.List;
+import java.util.HashMap;
 import java.util.Map;
-import java.util.TreeMap;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder stringBuilder = new StringBuilder();
 
         // color 값 저장
-        Map<String, List<String>> colors = new TreeMap<>() {
+        Map<String, Integer> colors = new HashMap<>() {
             {
-                put("black", List.of("0", ""));
-                put("brown", List.of("1", "0"));
-                put("red", List.of("2", "00"));
-                put("orange", List.of("3", "000"));
-                put("yellow", List.of("4", "0000"));
-                put("green", List.of("5", "00000"));
-                put("blue", List.of("6", "000000"));
-                put("violet", List.of("7", "0000000"));
-                put("grey", List.of("8", "00000000"));
-                put("white", List.of("9", "000000000"));
+                put("black", 0);
+                put("brown", 1);
+                put("red", 2);
+                put("orange", 3);
+                put("yellow", 4);
+                put("green", 5);
+                put("blue", 6);
+                put("violet", 7);
+                put("grey", 8);
+                put("white", 9);
             }
         };
-        
-        // 더해야 하는 값(= index 0) 찾아서 문자열로 붙이기
-        for (int i = 0; i < 2; i++) {
-            String color = bufferedReader.readLine();
-            stringBuilder.append(colors.entrySet().stream()
-                    .filter(e -> e.getKey().equals(color))
-                    .map(Map.Entry::getValue)
-                    .findFirst()
-                    .get()
-                    .get(0));
-        }
-        
-        // 곱해야 하는 값(= index 1) 찾아서 문자열로 붙이기
-        String color = bufferedReader.readLine();
-        stringBuilder.append(colors.entrySet().stream()
-                .filter(e -> e.getKey().equals(color))
-                .map(Map.Entry::getValue)
-                .findFirst()
-                .get()
-                .get(1));
-        
-        // 결과 출력(stringbuilder로 바로 출력하면 에러뜸)
-        System.out.println(Long.parseLong(stringBuilder.toString()));
+
+        // 입력
+        String color1 = bufferedReader.readLine();
+        String color2 = bufferedReader.readLine();
+        String color3 = bufferedReader.readLine();
+
+        long result = (colors.get(color1) * 10 + colors.get(color2)) * (long) Math.pow(10, colors.get(color3));
+
+        // 결과 출력
+        System.out.println(result);
     }
 }
