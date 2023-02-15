cf009c34b8cb971ead3e15ed5faa32895970d88d
Jinny-l <jinny.l.dev@gmail.com>
2023-02-15 23:57:59 +0900

    [level 0] Title: 세균 증식, Time: 0.06 ms, Memory: 77.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120910. 세균 증식/세균 증식.java
@@ -0,0 +1,11 @@
+import java.math.*;
+
+class Solution {
+    public double solution(int n, int t) {
+        double answer = 0;
+        
+        answer = Math.pow(2, t) * n;
+        
+        return answer;
+    }
+}
