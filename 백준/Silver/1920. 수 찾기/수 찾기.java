fc36f74fe5c71e1e20a780765da61cf15b8caae7
JeeIn Lee <jinny.l.dev@gmail.com>
2023-04-02 14:21:20 +0900

    [Silver IV] Title: 수 찾기, Time: 608 ms, Memory: 44580 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -1,50 +1,47 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
 
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer stringTokenizer;
-
-        // N과 숫자 입력받기
-        int N = Integer.parseInt(bufferedReader.readLine());
-        stringTokenizer = new StringTokenizer(bufferedReader.readLine(), " ");
-        int[] NNum = new int[N];
+        // N 입력 및 정렬
+        int N = Integer.parseInt(br.readLine());
+        int[] arrN = new int[N];
+        st = new StringTokenizer(br.readLine(), " ");
         for (int i = 0; i < N; i++) {
-            NNum[i] = Integer.parseInt(stringTokenizer.nextToken());
+            arrN[i] = Integer.parseInt(st.nextToken());
         }
-        Arrays.sort(NNum); // 이진 탐색을 위해 정렬
+        Arrays.sort(arrN);
 
-        // M과 숫자 입력받기
-        int M = Integer.parseInt(bufferedReader.readLine());
-
-        stringTokenizer = new StringTokenizer(bufferedReader.readLine(), " ");
-        int[] MNum = new int[M];
+        // M 입력
+        int M = Integer.parseInt(br.readLine());
+        int[] arrM = new int[M];
+        st = new StringTokenizer(br.readLine(), " ");
         for (int i = 0; i < M; i++) {
-            MNum[i] = Integer.parseInt(stringTokenizer.nextToken());
+            arrM[i] = Integer.parseInt(st.nextToken());
         }
 
-        // 겹치는 수 확인
-        List<Integer> result = new ArrayList<>();
-
+        // 이분탐색
+        int[] result = new int[M];
         for (int i = 0; i < M; i++) {
-            if (Arrays.binarySearch(NNum, MNum[i]) >= 0) {
-                result.add(1);
-                continue;
+            if (Arrays.binarySearch(arrN, arrM[i]) >= 0) { // 값이 있을 때
+                result[i] = 1;
+            } else {
+                result[i] = 0;
             }
-            result.add(0);
         }
-
+        
         // 결과 출력
-        for (Integer I : result) {
-            System.out.println(I);
+        StringBuilder sb = new StringBuilder();
+        for (int value : result) {
+            sb.append(value)
+                    .append("\n");
         }
+        System.out.print(sb);
     }
 }
-
