0e5425cd227c840e36aa071cef48c91411e48839
JeeIn Lee <jinny.l.dev@gmail.com>
2023-05-19 17:10:36 +0900

    [level 1] Title: K번째수, Time: 0.86 ms, Memory: 73.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42748. K번째수/K번째수.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+        
+        for (int i = 0; i < commands.length; i++) {
+            int start = commands[i][0] - 1;
+            int end = commands[i][1];
+            int index = commands[i][2] - 1;
+            
+            int[] tmp = Arrays.copyOfRange(array, start, end);
+            Arrays.sort(tmp);
+            answer[i] = tmp[index];
+        }
+        
+        return answer;
+    }
+}
