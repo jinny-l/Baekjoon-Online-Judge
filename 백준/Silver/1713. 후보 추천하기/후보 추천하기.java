77ff7602a9b1d870f3518c8c8cd622dc54255122
JeeIn Lee <jinny.l.dev@gmail.com>
2023-07-30 16:24:42 +0900

    [Silver I] Title: 후보 추천하기, Time: 160 ms, Memory: 16040 KB -BaekjoonHub

--- 백준/Silver/1713. 후보 추천하기/후보 추천하기.java
+++ 백준/Silver/1713. 후보 추천하기/후보 추천하기.java
@@ -1,77 +1,79 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.StringTokenizer;
-import java.io.IOException;
 
 public class Main {
-	
-	public static void main(String [] args) throws IOException {
-		BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		
-		int N, total;
-		Student students[]= new Student[101];
-		List<Student> list= new ArrayList<>();
-		
-		N= Integer.parseInt(br.readLine());
-		total= Integer.parseInt(br.readLine());
-		
-		st= new StringTokenizer(br.readLine());
-		int num;
-		for(int i=0; i<total; i++) {
-			num= Integer.parseInt(st.nextToken());
-            
-			if(students[num]==null) {
-				students[num]= new Student(num, 0, 0, false);
-			}
-			
-			if(students[num].isPosted) {
-				students[num].cnt++;
-			}
-			else {
-				if(list.size()==N) {
-          
-					Collections.sort(list, new Comparator<Student>() {
-						public int compare(Student o1, Student o2) {
-							if(o1.cnt==o2.cnt) {
-								return o1.time-o2.time;
-							}
-							return o1.cnt-o2.cnt;
-						}
-					});
-					list.get(0).isPosted=false;
-					list.remove(0);
-				}
-				students[num].cnt=1;
-				students[num].time=i;
-				students[num].isPosted=true;
-				list.add(students[num]);
-			}
-		}
-		
-		Collections.sort(list, (o1, o2) -> o1.idx-o2.idx);
-		for(Student i: list) {
-			System.out.print(i.idx+" ");
-		}
-		
-	}
-	
-	static class Student{
-		int idx; 
-		int cnt;
-		int time;
-		boolean isPosted; 
-		
-		public Student(int idx, int cnt, int time, boolean isPosted) {
-			this.idx = idx;
-			this.cnt = cnt;
-			this.time = time;
-			this.isPosted = isPosted;
-		}
-	}
-	
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        // 입력
+        int postSize = Integer.parseInt(br.readLine());
+        int votes = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        // 최종 후보 확인
+        Student[] students = new Student[101]; // 모든 학생의 정보를 담는 배열
+        List<Student> post = new ArrayList<>(); // 사진틀에 게시된 최종 후보의 학생 정보를 담는 배열
+
+        for (int i = 0; i < votes; i++) {
+            int vote = Integer.parseInt(st.nextToken());
+
+            if (students[vote] == null) { // 처음 추천받았다면 객체 생성
+                students[vote] = new Student(vote, 0, 0, false);
+            }
+
+            if (students[vote].isPosted) {  // 이미 게시되어 있으면 추천수 1증가하고, 시간 업데이트
+                students[vote].count++;
+                continue;
+            }
+
+            if (post.size() < postSize) {    // 사진틀이 비어있으면
+                post.add(students[vote]);       // 게시하고
+
+            } else {                          // 사진들이 차있으면
+                Collections.sort(post);       // 추천순, 시간순으로 정렬하고
+                post.get(0).isPosted = false; // 게시 여부 정보 바꾸고
+                post.get(0).count = 0;        // 추천 수 0으로 변경
+                post.remove(0);         // 기존 학생 제거하고
+                post.add(students[vote]);     // 새로운 학생 게시
+            }
+
+            // 추천된 학생 정보 업데이트
+            students[vote].count++;         // 추천수 1 증가하고
+            students[vote].time = i;        // 포스팅 시간 업데이트하고
+            students[vote].isPosted = true; // 포스팅 여부 업데이트
+        }
+
+        // 출력
+        post.sort(Comparator.comparingInt(o -> o.index));
+        post.forEach(student -> System.out.print(student.index + " "));
+    }
+
+    static class Student implements Comparable<Student> {
+        int index;
+        int count;
+        int time;
+        boolean isPosted;
+
+        public Student(int index, int count, int time, boolean isPosted) {
+            this.index = index;
+            this.count = count;
+            this.time = time;
+            this.isPosted = isPosted;
+        }
+
+        @Override
+        public int compareTo(Student o) {
+            if (this.count == o.count) {
+                return this.time - o.time;
+            }
+            return this.count - o.count;
+        }
+    }
 }
