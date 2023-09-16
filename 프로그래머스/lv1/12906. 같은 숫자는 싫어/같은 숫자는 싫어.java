bcfe0527e72d5c362b825805591b3e1be98c2b1b
JeeIn Lee <jinny.l.dev@gmail.com>
2023-09-16 22:15:15 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 32.14 ms, Memory: 136 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.util.stream.*;
+
+public class Solution {
+    public int[] solution(int []arr) {
+        List<Integer> answer = new ArrayList<>();
+        
+        answer.add(arr[0]);
+        for(int i = 1; i < arr.length; i++) {
+            if(arr[i] == arr[i-1]) {
+                continue;
+            }
+            answer.add(arr[i]);
+        }
+
+        return answer.stream()
+                .mapToInt(i -> i)
+                .toArray();
+    }
+}
