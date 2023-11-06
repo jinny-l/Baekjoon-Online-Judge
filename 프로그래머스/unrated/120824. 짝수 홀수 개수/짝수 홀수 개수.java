16f6cb0b2ab80b14b501687506f2617b532c9edf
Lee Jee In <jinny.l.dev@gmail.com>
2023-11-06 20:32:33 +0900

    [level unrated] Title: 짝수 홀수 개수, Time: 0.03 ms, Memory: 79 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120824. 짝수 홀수 개수/짝수 홀수 개수.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int[] solution(int[] num_list) {
+        int even = 0;
+        
+        for(int i = 0; i < num_list.length; i++) {
+            if(num_list[i] % 2 == 0) even++;
+        }
+        
+        return new int[]{even, num_list.length - even};
+    }
+}
