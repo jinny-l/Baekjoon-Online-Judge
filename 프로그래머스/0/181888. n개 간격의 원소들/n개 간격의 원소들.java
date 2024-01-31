2d990f2bf936cab3f09ebdc19b4ee8616c262b25
Lee Jee In <jinny.l.dev@gmail.com>
2024-01-31 17:03:55 +0900

    [level 0] Title: n개 간격의 원소들, Time: 0.19 ms, Memory: 76 MB -BaekjoonHub

--- 프로그래머스/0/181888. n개 간격의 원소들/n개 간격의 원소들.java
+++ 프로그래머스/0/181888. n개 간격의 원소들/n개 간격의 원소들.java
@@ -1,14 +1,14 @@
 class Solution {
     public int[] solution(int[] num_list, int n) {
-        // int[] answer = new int[(int) Math.round((double) num_list.length / n)];
+        int[] answer = new int[(int) Math.ceil((double) num_list.length / n)];
         
-        int[] answer = {};
+        // int[] answer = {};
         
-        if (num_list.length % n == 0) {
-            answer = new int[num_list.length / n];
-        } else {
-            answer = new int[num_list.length / n + 1];
-        }
+        // if (num_list.length % n == 0) {
+        //    answer = new int[num_list.length / n];
+        // } else {
+        //    answer = new int[num_list.length / n + 1];
+        //}
         
         int index = 0;
         for(int i = 0; i < num_list.length; i += n) {
