85d53f7cda6602e5c2fdcf7f4f9b3109ed2dbb27
Lee Jee In <jinny.l.dev@gmail.com>
2024-03-31 13:24:28 +0900

    [level 0] Title: 배열 비교하기, Time: 0.01 ms, Memory: 76.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181856. 배열 비교하기/배열 비교하기.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int solution(int[] arr1, int[] arr2) {
+        // 배열의 길이가 같을 때, 배열의 합 비교
+        if(arr1.length == arr2.length) {
+            int sum1 = 0;
+            int sum2 = 0;
+            
+            for(int i = 0; i < arr1.length; i++) {
+                sum1 += arr1[i];
+                sum2 += arr2[i];
+            }
+            
+            if(sum1 == sum2) {
+                return 0;
+            
+            } else if(sum1 > sum2) {
+                return 1;
+            
+            } else {
+                return -1;
+            }
+        }
+        
+        
+        // 배열의 길이가 다를 때, 길이가 더 긴쪽이 큼
+        if(arr1.length > arr2.length) {
+            return 1;
+        }
+        
+        return -1;
+    }
+}
