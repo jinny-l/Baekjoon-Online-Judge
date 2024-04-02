8fbed8a72bb35ee1c3ed51858248780a177fb6f2
Lee Jee In <jinny.l.dev@gmail.com>
2024-04-02 13:37:22 +0900

    [level 0] Title: 배열 만들기 5, Time: 3.24 ms, Memory: 93.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181912. 배열 만들기 5/배열 만들기 5.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(String[] intStrs, int k, int s, int l) {
+        List<Integer> list = new ArrayList<>();
+        
+        for(String val : intStrs) {
+            int num = Integer.parseInt(val.substring(s, s + l));
+            
+            if(num > k) {
+                list.add(num);
+            }
+        }
+        
+        int[] result = new int[list.size()];
+        
+        for (int i = 0; i < list.size(); i++) {
+            result[i] = list.get(i);
+        }
+        
+        return result;
+    }
+}
