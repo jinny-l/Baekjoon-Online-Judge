ee742b8f441c83fa8d74ef06b20f4bbed750dc99
Lee Jee In <jinny.l.dev@gmail.com>
2024-04-09 16:09:03 +0900

    [level 0] Title: 문자 개수 세기, Time: 0.04 ms, Memory: 73.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181902. 문자 개수 세기/문자 개수 세기.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] solution(String my_string) {
+        int[] answer = new int[52];
+        
+        for(int i = 0; i < my_string.length(); i++) {
+            char c = my_string.charAt(i);
+            
+            if(Character.isUpperCase(c)) {
+                answer[c - 'A']++;
+            }
+            
+            if(Character.isLowerCase(c)) {
+                answer[c - 'a' + 26]++;
+            }
+        }
+        
+        return answer;
+    }
+}
