ef513fa7ac752ddd21842dab3ba3d57184f5ff0b
Lee Jee In <jinny.l.dev@gmail.com>
2024-04-11 14:23:34 +0900

    [level 0] Title: 수열과 구간 쿼리 3, Time: 0.05 ms, Memory: 74 MB -BaekjoonHub

---
+++ 프로그래머스/0/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] solution(int[] arr, int[][] queries) {
+        for(int i = 0; i < queries.length; i++) {
+            int[] query = queries[i];
+            
+            int tmp = arr[query[0]];
+            arr[query[0]] = arr[query[1]];
+            arr[query[1]] = tmp;
+        }
+        
+        return arr;
+    }
+}
