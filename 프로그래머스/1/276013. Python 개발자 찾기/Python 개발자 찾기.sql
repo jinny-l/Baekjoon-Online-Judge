fcbf9be80f504b91bd9b16fcd6f4d35768f17ac8
Lee Jee In <jinny.l.dev@gmail.com>
2024-04-19 14:22:52 +0900

    [level 1] Title: Python 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/1/276013. Python 개발자 찾기/Python 개발자 찾기.sql
@@ -0,0 +1,15 @@
+-- Python 스킬을 가진 개발자의 ID, 이메일, 이름, 성을 조회하는 SQL 문
+-- ID를 기준으로 오름차순 정렬
+
+SELECT 
+    ID, EMAIL, FIRST_NAME, LAST_NAME
+FROM
+    DEVELOPER_INFOS
+WHERE 
+    SKILL_1 = 'Python'
+OR
+    SKILL_2 = 'Python'
+OR
+    SKILL_3 = 'Python'
+ORDER BY
+    ID;
