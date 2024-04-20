9283d42f07c66c5c2840139ff7e7474fc2ab8260
Lee Jee In <jinny.l.dev@gmail.com>
2024-04-20 19:36:44 +0900

    [level 1] Title: 조건에 부합하는 중고거래 댓글 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/1/164673. 조건에 부합하는 중고거래 댓글 조회하기/조건에 부합하는 중고거래 댓글 조회하기.sql
@@ -0,0 +1,23 @@
+-- 2022년 10월에 작성된 게시글 제목, 게시글 ID, 댓글 ID, 댓글 작성자 ID, 댓글 내용, 댓글 작성일을 조회
+-- 결과는 댓글 작성일을 기준으로 오름차순 정렬해주시고, 댓글 작성일이 같다면 게시글 제목을 기준으로 오름차순 정렬
+
+SELECT
+    TITLE,
+    BOARD.BOARD_ID AS BOARD_ID,
+    REPLY_ID,
+    REPLY.WRITER_ID AS WRITER_ID,
+    REPLY.CONTENTS AS CONTENTS,
+    DATE_FORMAT(REPLY.CREATED_DATE, '%Y-%m-%d') AS CREATED_DATE
+FROM
+    USED_GOODS_REPLY REPLY
+    LEFT JOIN
+    USED_GOODS_BOARD BOARD
+    ON
+    REPLY.BOARD_ID = BOARD.BOARD_ID
+WHERE
+    YEAR(BOARD.CREATED_DATE) = 2022
+    AND
+    MONTH(BOARD.CREATED_DATE) = 10
+ORDER BY
+    REPLY.CREATED_DATE, TITLE  
+;
