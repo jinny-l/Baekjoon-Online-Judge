2b38d11cfe6f977d2205abbc6efe6adff7a1c010
Lee Jee In <jinny.l.dev@gmail.com>
2024-04-21 11:30:00 +0900

    [level 0] Title: 합성수 찾기, Time: 0.01 ms, Memory: 74.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120846. 합성수 찾기/합성수 찾기.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int solution(int n) {
+        if(n < 4) return 0;
+        
+        int answer = 0;
+        
+        for(int i = 4; i <= n; i++) {
+            if(count(i) >= 3) {
+                answer++;
+            }
+        }
+        
+        return answer;
+    }
+    
+    private int count(int n) {
+        int cnt = 0;
+        
+        for(int i = 1; i <= n; i++) {
+            if(n % i == 0) {
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+}
