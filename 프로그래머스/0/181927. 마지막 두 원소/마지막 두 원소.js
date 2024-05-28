577af3ab7a0110483ca78e0d232b445503443530
Lee Jee In <jinny.l.dev@gmail.com>
2024-05-28 14:04:53 +0900

    [level 0] Title: 마지막 두 원소, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.js
@@ -0,0 +1,12 @@
+function solution(num_list) {
+    var answer = num_list;
+    
+    if(num_list[num_list.length - 1] - num_list[num_list.length - 2] > 0) {
+        answer.push(num_list[num_list.length - 1] - num_list[num_list.length - 2]);
+    
+    } else {
+        answer.push(num_list[num_list.length - 1] * 2);
+    }
+    
+    return answer;
+}
