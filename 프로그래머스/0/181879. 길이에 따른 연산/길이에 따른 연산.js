64484eff6f30ac36aaa750a16e11312c05c4bec3
Lee Jee In <jinny.l.dev@gmail.com>
2024-05-29 10:38:01 +0900

    [level 0] Title: 길이에 따른 연산, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181879. 길이에 따른 연산/길이에 따른 연산.js
@@ -0,0 +1,8 @@
+function solution(num_list) {
+    if (num_list.length >= 11) {
+        return num_list.reduce((sum, num) => sum + num, 0);
+    
+    } else {
+        return num_list.reduce((sum, num) => sum * num, 1);
+    }
+}
