4153c9bc920d9b545e0aca26b92e98fab77736d6
Lee Jee In <jinny.l.dev@gmail.com>
2024-06-03 10:17:15 +0900

    [level 0] Title: 리스트 자르기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181897. 리스트 자르기/리스트 자르기.js
@@ -0,0 +1,15 @@
+function solution(n, slicer, num_list) {
+    if (n === 1) {
+        return num_list.slice(0, slicer[1] + 1);
+    }
+    
+    if (n === 2) {
+        return num_list.slice(slicer[0]);
+    }
+    
+    if (n === 3) {
+        return num_list.slice(slicer[0], slicer[1] + 1);
+    }
+    
+    return num_list.slice(slicer[0], slicer[1] + 1).filter((_, index) => index % slicer[2] === 0);
+}
